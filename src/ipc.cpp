8e188eb22c4475fa4e1d53f540af2d2d768289a2
Muqiu Han <muqiu-han@outlook.com>
2023-08-02 17:27:14 +0800

    Update: Change IPC usage

--- src/ipc.cpp
+++ src/ipc.cpp
@@ -4,28 +4,28 @@
 namespace bonding::ipc
 {
   Result<Unit, error::Err>
-  IPC::send_boolean(bool data) const noexcept
+  IPC::send_boolean(const int socket, const bool data) noexcept
   {
     const uint8_t buf[1] = { data };
 
     /* With a zero flags argument, send() is equivalent to write(). */
-    if (-1 == send(m_socket, &buf, 1, 0))
+    if (-1 == send(socket, &buf, 1, 0))
       {
-        spdlog::error("Cannot send boolean through socket: {}", m_socket);
+        spdlog::error("Cannot send boolean through socket: {}", socket);
         return Err(bonding::error::Err(bonding::error::Code::SocketError));
       }
     return Ok(Unit());
   }
 
   Result<bool, error::Err>
-  IPC::recv_boolean(bool data) const noexcept
+  IPC::recv_boolean(const int socket) noexcept
   {
     uint8_t buf[1] = { 0 };
 
     /* With a zero flags argument, recv() is generally equivalent to read() */
-    if (-1 == recv(m_socket, &buf, 1, 0))
+    if (-1 == recv(socket, &buf, 1, 0))
       {
-        spdlog::error("Cannot recevie boolean from socket {}", m_socket);
+        spdlog::error("Cannot recevie boolean from socket {}", socket);
         return Err(bonding::error::Err(bonding::error::Code::SocketError));
       }
 
