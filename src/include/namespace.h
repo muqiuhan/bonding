d76551484daade423344f81ed6fe4a0fa3d3ea7d
Muqiu Han <muqiu-han@outlook.com>
2023-08-02 18:46:12 +0800

    Add: Namespace support

--- src/include/namespace.h
+++ src/include/namespace.h
@@ -3,11 +3,49 @@
 
 #include "error.h"
 #include "result.hpp"
+#include <sched.h>
+#include <sys/types.h>
 
 namespace bonding::ns
 {
+  /** User namespaces allows process to virtually be root inside their
+   ** own namespace, but not on the parent system.
+   ** It also allows the child namespace to have its own users / groups configuration. */
   class Namespace
   {
+   public:
+    Namespace(const int socket, const uid_t uid)
+      : m_socket(socket)
+      , m_uid(uid)
+      , m_gid(uid)
+      , m_groups(uid)
+    {
+    }
+
+    /** Executed by the child process during its configuration. */
+    Result<Unit, error::Err> setup() const noexcept;
+
+    /** Called by the container when it will perform UID / GID mapping. */
+    static Result<Unit, error::Err> handle_child_uid_map(const pid_t pid,
+                                                         const int socket) noexcept;
+
+   private:
+    /** If that call is successful, then user namespaces are supported. */
+    Result<bool, error::Err> has_user_namespace() const noexcept;
+    static Result<Unit, error::Err> create_map(const int id,
+                                               const std::string map) noexcept;
+
+   private:
+    const int m_socket;
+    const uid_t m_uid;
+    const gid_t m_gid;
+    const gid_t m_groups[1];
+
+    /** mapped by the system to a real UID >10000,
+     ** and it can manage its users and groups
+     ** without polluting the parent system. */
+    static inline const uint64_t USERNS_OFFSET = 10000;
+    static inline const uint64_t USERNS_COUNT = 2000;
   };
 };
 
--- src/namespace.cpp
+++ src/namespace.cpp
@@ -1,6 +1,94 @@
 #include "include/namespace.h"
+#include "include/ipc.h"
+#include <fcntl.h>
+#include <grp.h>
+#include <sched.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 namespace bonding::ns
 {
+  Result<bool, error::Err>
+  Namespace::has_user_namespace() const noexcept
+  {
+    /* Unshare the user namespace, so that the calling process is
+     * moved into a new user namespace which is not shared with
+     * any previously existing process. */
+    if (-1 == unshare(CLONE_NEWUSER))
+      return Ok(false);
+
+    return Ok(true);
+  }
+
+  Result<Unit, error::Err>
+  Namespace::setup() const noexcept
+  {
+    spdlog::debug("Setting up user namespace with UID {}", m_uid);
+    const bool has_userns = has_user_namespace().unwrap();
+
+    ipc::IPC::send_boolean(m_socket, has_userns).unwrap();
+    if (ipc::IPC::recv_boolean(m_socket).unwrap())
+      return Err(bonding::error::Err(bonding::error::Code::NamespaceError));
+
+    if (has_userns)
+      spdlog::info("User namespace setup...");
+    else
+      spdlog::warn("User namespace not supported, continuting...");
+
+    /* set the list of groups the process is part of */
+    if (-1 == setgroups(1, m_groups))
+      return Err(bonding::error::Err(bonding::error::Code::NamespaceError));
+
+    /* set the UID and GID (respectively) of the process. 
+     * this will set the real user ID, the effective user ID,
+     *  and the saved set-user-ID. */
+    if (-1 == setresgid(m_gid, m_gid, m_gid))
+      return Err(bonding::error::Err(bonding::error::Code::NamespaceError));
+
+    if (-1 == setresuid(m_uid, m_uid, m_uid))
+      return Err(bonding::error::Err(bonding::error::Code::NamespaceError));
+
+    return Ok(Unit());
+  }
+
+  Result<Unit, error::Err>
+  Namespace::create_map(const int id, const std::string map) noexcept
+  {
+    const std::string path = "/proc/" + std::to_string(id) + "/" + map;
+    const int fd = creat(path.c_str(), 0777);
+    if (-1 == fd)
+      return Err(error::Err(error::Code::NamespaceError));
+
+    const std::string data =
+      "0 " + std::to_string(USERNS_OFFSET) + std::to_string(USERNS_COUNT);
+
+    if (-1 == write(fd, data.c_str(), data.size() -1))
+      return Err(error::Err(error::Code::NamespaceError));
+
+    return Ok(Unit());
+  }
+
+  Result<Unit, error::Err>
+  Namespace::handle_child_uid_map(const pid_t pid, const int socket) noexcept
+  {
+    if (ipc::IPC::recv_boolean(socket).unwrap())
+      {
+        create_map(pid, "uid_map").unwrap();
+        create_map(pid, "gid_map").unwrap();
+
+        return Ok(Unit());
+      }
+    else
+      {
+        spdlog::warn("No user namespace set up from child process");
+        return Ok(Unit());
+      }
+
+    spdlog::debug("Child UID/GID map done, sending signal to child to continue...");
+    ipc::IPC::send_boolean(socket, false);
+
+    return Ok(Unit());
+  }
 
 }
