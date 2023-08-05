f62ebe8de381ea8b03ffb8a925b1d331b69114fe
Muqiu Han <muqiu-han@outlook.com>
2023-08-06 07:20:54 +0800

    Add: Applying seccomp

--- src/syscall.cpp
+++ src/syscall.cpp
@@ -1,10 +1,21 @@
 #include "include/syscall.h"
+#include <libseccomp/seccomp.h>
 
-namespace bonding::syscall {
+namespace bonding::syscall
+{
   Result<Unit, error::Err>
   Syscall::setup() const noexcept
   {
     spdlog::debug("Refusing / Filtering unwanted syscalls");
-    
+
+    /* Initialize seccomp profile with all syscalls allowed by default */
+    const scmp_filter_ctx ctx = seccomp_init(SCMP_ACT_ALLOW);
+    if (NULL == ctx)
+      return Err(error::Err(error::Code::SystemcallError, "seccomp_init error"));
+
+    if (0 != seccomp_load(ctx))
+      return Err(error::Err(error::Code::SystemcallError, "seccomp_load error"));
+
+    return Ok(Unit());
   }
 }
