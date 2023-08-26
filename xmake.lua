9330642dca91a4e4c2933f82fd35e428fd25b401
Muqiu Han <muqiu-han@outlook.com>
2023-08-26 12:53:05 +0800

    Refactor: Remove  from the source tree

--- xmake.lua
+++ xmake.lua
@@ -5,17 +5,6 @@ add_requires("libcap")
 add_requires("libseccomp")
 add_requires("spdlog")
 
-package("result")
-    add_deps("cmake")
-    set_sourcedir(path.join(os.scriptdir(), "lib/result"))
-    on_install(function (package)
-        local configs = {}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        import("package.tools.cmake").install(package, configs)
-    end)
-package_end()
-
 package("configor")
     add_deps("cmake")
     set_sourcedir(path.join(os.scriptdir(), "lib/configor"))
@@ -32,7 +21,7 @@ target("bonding")
     set_languages("c++17")
 
     add_files("src/*.cpp")
-    add_includedirs("lib/result/include", "lib/configor/include")
+    add_includedirs("lib/configor/include")
 
     add_packages("spdlog", "result")
     add_links("seccomp", "cap")
