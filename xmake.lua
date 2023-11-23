598f3915a2a66dc2ec7a9405de83c30d43a18e55
Muqiu Han <muqiu-han@outlook.com>
2023-11-23 20:39:48 +0800

    [Update]: Smaller executable file

--- xmake.lua
+++ xmake.lua
@@ -5,12 +5,14 @@ set_xmakever("2.8.1")
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = "."})
 
-add_requires("libcap", "libseccomp", "plog", "nlohmann_json")
+add_requires("plog", "nlohmann_json")
 
 target("bonding")
     set_kind("binary")
     set_languages("c++17")
+    set_policy("check.auto_ignore_flags", false)
 
+    add_cxxflags("-O2 -ffunction-sections -Wl,-gc-sections -static-libstdc++ -s")
     add_files("src/*.cpp")
     add_packages("plog", "result", "nlohmann_json")
     add_links("seccomp", "cap")
